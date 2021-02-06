466741b76955a56f028e31ae28425d3e4c28aa23
Latte723 <69175961+Latte723@users.noreply.github.com>
2021-02-06 15:40:33 +0900

    loginバグ修正

--- js/login-modal.js
+++ js/login-modal.js
@@ -1,28 +1,30 @@
 var VirtualBandung = VirtualBandung || {};
 VirtualBandung.Login = VirtualBandung.Login || {};
 
-(() => {
-  const Selector = {
-    LOGIN_AREA: '#loginArea',
-    FORM: '#form',
-    EMAIL: '#mailAddress',
-    SIGNUP_FIELDS: '#signupFields',
-    AGE: '#age',
-    BIRTHPLACE: '#birthplace',
-    RESIDENCE: '#residence',
-    SEX: '#sex',
-    MODE: '#mode',
-    SIGNUP: '.login-contents__signup',
-    ENTER_BTN: '#enterBtn',
-    FORM_INPUT: '[name="formInput"]',
-    MSG: '#msg',
+(() => {  
+  const ElementId = {
+    LOGIN_AREA: 'loginArea',
+    FORM: 'form',
+    EMAIL: 'mailAddress',
+    SIGNUP_FIELDS: 'signupFields',
+    AGE: 'age',
+    BIRTHPLACE: 'birthplace',
+    RESIDENCE: 'residence',
+    SEX: 'sex',
+    MODE: 'mode',
+    ENTER_BTN: 'enterBtn',
+    MSG: 'msg',
     Message: {
-      EMAIL: '#mailAddressMsg',
-      AGE: '#ageMsg',
-      BIRTHPLACE: '#birthplaceMsg',
-      RESIDENCE: '#residenceMsg',
-      SEX: '#sexMsg'
+      EMAIL: 'mailAddressMsg',
+      AGE: 'ageMsg',
+      BIRTHPLACE: 'birthplaceMsg',
+      RESIDENCE: 'residenceMsg',
+      SEX: 'sexMsg'
     }
+  };
+
+  const ElementName = {
+    FORM_INPUT: 'formInput'
   };
 
   const CssClass = {
@@ -59,10 +61,10 @@ VirtualBandung.Login = VirtualBandung.Login || {};
   const countrySet = new Set(["Japan", "Indonesia", "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Costa Rica", "Côte d’Ivoire", "Croatia", "Cuba", "Cyprus", "Czechia", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Holy See", "Honduras", "Hungary", "Iceland", "India", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Republic of the Congo", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "São Tomé and Príncipe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"]);
   const sexMap = new Map([['1', 'Male'], ['2', 'Female'], ['3', 'Other']]);
 
-  const mailAddressPattern = new RegExp(/^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/);
+  const mailAddressPattern = new RegExp(/^[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/);
   
-  function createOptions(selector, list) {
-    const select = document.querySelector(selector);
+  function createOptions(id, list) {
+    const select = document.getElementById(id);
 
     updateSelectFont(select);
     select.addEventListener('change', e => updateSelectFont(e.currentTarget));
@@ -77,10 +79,16 @@ VirtualBandung.Login = VirtualBandung.Login || {};
   }
 
   function createSignUpContents() {
-    createOptions(Selector.AGE, ageMap);
-    createOptions(Selector.BIRTHPLACE, countrySet);
-    createOptions(Selector.RESIDENCE, countrySet);
-    createOptions(Selector.SEX, sexMap);
+    createOptions(ElementId.AGE, ageMap);
+    createOptions(ElementId.BIRTHPLACE, countrySet);
+    createOptions(ElementId.RESIDENCE, countrySet);
+    createOptions(ElementId.SEX, sexMap);
+  }
+
+  function updateMailAddress() {
+    const mailAddress = document.getElementById(ElementId.EMAIL);
+    const {value} = mailAddress;
+    mailAddress.value = value.replace(/\s/g, '');
   }
   
   function fade(node, duration, mode) {
@@ -124,15 +132,15 @@ VirtualBandung.Login = VirtualBandung.Login || {};
   }
 
   function showSignupFields() {
-    const signupFields = document.querySelector(Selector.SIGNUP_FIELDS);
+    const signupFields = document.getElementById(ElementId.SIGNUP_FIELDS);
     if (signupFields.classList.contains(CssClass.HIDDEN)) {
       signupFields.classList.remove(CssClass.HIDDEN);
     }
   }
 
-  function checkField(fieldSelector, msgSelector, test) {
-    const {value} = document.querySelector(fieldSelector);
-    const {classList: msgClassList} = document.querySelector(msgSelector);
+  function checkField(fieldId, msgId, test) {
+    const {value} = document.getElementById(fieldId);
+    const {classList: msgClassList} = document.getElementById(msgId);
     if (test(value)) {
       if (!msgClassList.contains(CssClass.HIDDEN)) {
         msgClassList.add(CssClass.HIDDEN);
@@ -145,26 +153,28 @@ VirtualBandung.Login = VirtualBandung.Login || {};
   }
 
   function checkMailAddress() {
-    return checkField(Selector.EMAIL, Selector.Message.EMAIL, (value) => mailAddressPattern.test(value));
+    return checkField(ElementId.EMAIL, ElementId.Message.EMAIL, (value) => mailAddressPattern.test(value));
   }
 
-  function checkSelectField(fieldSelector, msgSelector) {
+  function checkSelectField(fieldId, msgId) {
     return VirtualBandung.Login.mode === SendMode.LOGIN
-        || checkField(fieldSelector, msgSelector, (value) => !!value);
+        || checkField(fieldId, msgId, (value) => !!value);
   }
 
   function checkInput() {
-    return checkMailAddress()
-        && checkSelectField(Selector.AGE, Selector.Message.AGE)
-        && checkSelectField(Selector.BIRTHPLACE, Selector.Message.BIRTHPLACE)
-        && checkSelectField(Selector.RESIDENCE, Selector.Message.RESIDENCE)
-        && checkSelectField(Selector.SEX, Selector.Message.SEX);
+    return [
+      checkMailAddress(),
+      checkSelectField(ElementId.AGE, ElementId.Message.AGE),
+      checkSelectField(ElementId.BIRTHPLACE, ElementId.Message.BIRTHPLACE),
+      checkSelectField(ElementId.RESIDENCE, ElementId.Message.RESIDENCE),
+      checkSelectField(ElementId.SEX, ElementId.Message.SEX)
+    ].every(result => result);
   }
 
   function sendForm() {
     if (!checkInput()) return false;
 
-    const queryParams = Array.from(document.querySelectorAll(Selector.FORM_INPUT))
+    const queryParams = Array.from(document.getElementsByName(ElementName.FORM_INPUT))
         .filter(input => input)
         .reduce((params, element) => {
           params.append(element.id, element.value || '');
@@ -178,7 +188,7 @@ VirtualBandung.Login = VirtualBandung.Login || {};
             VirtualBandung.Login.id = response.id;
             window.sessionStorage.setItem([SessionKey.ID], [VirtualBandung.Login.id])
             VirtualBandung.Login.msg = '';
-            const loginArea = document.querySelector(Selector.LOGIN_AREA);
+            const loginArea = document.getElementById(ElementId.LOGIN_AREA);
             fade(loginArea, 500, FadeMode.OUT);
             break;
           case Result.NG:
@@ -186,11 +196,11 @@ VirtualBandung.Login = VirtualBandung.Login || {};
             VirtualBandung.Login.id = 0;
             VirtualBandung.Login.msg = response.msg;
 
-            const modeElement = document.querySelector(Selector.MODE);
+            const modeElement = document.getElementById(ElementId.MODE);
             showSignupFields();
             modeElement.value = VirtualBandung.Login.mode = SendMode.SIGNUP;
 
-            const msgElement = document.querySelector(Selector.MSG);
+            const msgElement = document.getElementById(ElementId.MSG);
             const msgText = document.createTextNode(VirtualBandung.Login.msg);
             msgElement.innerHTML = '';
             msgElement.appendChild(msgText);
@@ -201,14 +211,17 @@ VirtualBandung.Login = VirtualBandung.Login || {};
   }
   
   function init() {
-    const loginArea = document.querySelector(Selector.LOGIN_AREA);
+    const loginArea = document.getElementById(ElementId.LOGIN_AREA);
     fade(loginArea, 500, FadeMode.IN);
 
-    const enterBtn = document.querySelector(Selector.ENTER_BTN);
-    enterBtn.addEventListener('click', sendForm);
-
-    const form = document.querySelector(Selector.FORM);
+    const form = document.getElementById(ElementId.FORM);
     form.addEventListener('submit', sendForm);
+
+    const mailAddress = document.getElementById(ElementId.EMAIL);
+    mailAddress.addEventListener('blur', updateMailAddress);
+
+    const enterBtn = document.getElementById(ElementId.ENTER_BTN);
+    enterBtn.addEventListener('click', sendForm);
 
     VirtualBandung.Login.mode = SendMode.LOGIN;
   }
@@ -217,5 +230,8 @@ VirtualBandung.Login = VirtualBandung.Login || {};
   if (!VirtualBandung.Login.id) {
     init();
     createSignUpContents();
+  } else {
+    const loginArea = document.getElementById(ElementId.LOGIN_AREA);
+    loginArea.style.display = 'none';
   }
 })(this);
